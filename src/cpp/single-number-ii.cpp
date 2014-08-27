575577becbefedcdbdf2888e1c3a74cc2fef02c1
landhero <raindrops_fall@sina.com>
2014-08-27 11:25:38 +0800

    update single number ii

--- src/cpp/single-number-ii.cpp
+++ src/cpp/single-number-ii.cpp
@@ -1,18 +1,20 @@
 /**
-* @author Hansheng Zhang (http://zhanghs.com)
+* Single Number II
+* Given an array of integers, every element appears three times except for one. Find that single one.
+* Solution: use three number to record bits
 */
+
 class Solution {
 public:
-    int singleNumber(int[], int);
+    int singleNumber(int A[], int n);
 };
+
 int Solution::singleNumber(int A[], int n){
-    int ones = 0, twos = 0, nthrees = 0;
-    for (int i=0; i<n; i++){
-        nthrees = ~(twos & A[i]);
-        twos = twos | (ones & A[i]);
-        ones = ones ^ A[i];
-        twos = twos & nthrees;
-        ones = ones & nthrees;
-    }
-    return ones;
+	int bits_3 = 0, bits_2 = 0, bits_1 = 0;
+	for (int i=0; i<n; i++){
+		bits_3 = bits_3 & ~A[i] | bits_2 & A[i];
+		bits_2 = bits_1 & A[i] | bits_2 & ~A[i];
+		bits_1 = bits_1 & ~A[i] | A[i] & ~bits_3 & ~bits_2;
+	}
+	return bits_1;
 }
