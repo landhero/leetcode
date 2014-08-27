ad39fb56145fe344ddba3a12110b949a71be06c8
landhero <raindrops_fall@sina.com>
2014-08-27 15:31:07 +0800

    update word break

--- src/cpp/word-break.cpp
+++ src/cpp/word-break.cpp
@@ -1,39 +1,25 @@
 /**
-* @author Hansheng Zhang (http://zhanghs.com)
+* Word Break
+* Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+* Solution: dynamic programming
 */
+
 class Solution {
 public:
-    bool wordBreak(string &, int, unordered_set<string> &, int*);
-    bool wordBreak(string, unordered_set<string>&);
+    bool wordBreak(string s, unordered_set<string> &dict);
 };
 
-bool Solution::wordBreak(string &s, int index, unordered_set<string> &dict, int* flag){
-    if (flag[index]!=0)
-        return flag[index]==1 ? true:false;
-    int len = s.length()-index;
-    string header;
-    bool ans = false;
-    for (int i=index; i<s.length(); i++){
-        header = header+s.at(i);
-        if (dict.find(header)!=dict.end())
-            if (wordBreak(s, i+1, dict,flag)){
-                ans = true;
-                break;
-            }
-    }
-    if (ans)
-        flag[index] = 1;
-    else flag[index] = -1;
-    return ans;
-}
-
-bool Solution::wordBreak(string s, unordered_set<string> &dict) {
-    // Note: The Solution object is instantiated only once and is reused by each test case.
-    int* flag = new int[s.length()+1];
-    flag[s.length()] = 1;
-    for (int i=0; i<s.length(); i++)
-        flag[i] = 0;
-    bool ans = wordBreak(s, 0, dict, flag);
-    delete []flag;
-    return ans;
+bool Solution::wordBreak(string s, unordered_set<string> &dict){
+	int n = s.length();
+	if (n<=0) return true;
+	vector<bool> flag(n+1, false);
+	flag[0] = true;
+	for (int i =0; i<n; i++){
+		if ( !flag[i] ) continue;
+		for (int j=i+1; j<=n; j++){
+			if (dict.find(s.substr(i,j-i))!=dict.end())
+				flag[j] = true;
+		}
+	}
+	return flag[n];
 }
