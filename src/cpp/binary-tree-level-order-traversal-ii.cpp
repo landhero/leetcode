7cc813c99d31973b9fb5596070a6241766e3c140
landhero <raindrops_fall@sina.com>
2014-09-04 21:48:10 +0800

    update Binary Tree Level Order Traversal II

--- src/cpp/binary-tree-level-order-traversal-ii.cpp
+++ src/cpp/binary-tree-level-order-traversal-ii.cpp
@@ -1,42 +1,35 @@
 /**
-  * @author Hansheng Zhang (http://zhanghs.com)
-  */
- class Solution {
- public:
-     vector<vector<int> > levelOrderBottom(TreeNode *root) {
-         // Start typing your C/C++ solution below
-         // DO NOT write int main() function
-       vector<vector<int>> rlt;
-       if (root == NULL)
-         return rlt;
-       queue<TreeNode*> vt;
-       vector<int> vi;
-       vt.push(root);
-       vt.push(NULL);
-       TreeNode * node;
-       while(!vt.empty()){
-         node = vt.front();
-         if (node==NULL){
-           if (vi.size()>0){
-             rlt.push_back(vi);
-             vi = vector<int>();
-             vt.push(NULL);
-           }
-         }
-         else{
-           vi.push_back(node->val);
-           if (node->left != NULL)
-             vt.push(node->left);
-           if (node->right != NULL)
-             vt.push(node->right);
-         }
-         vt.pop();
-       }
-       vector<vector<int>> real_rlt;
-       vector<vector<int>>::iterator vii;
-       for (vii=rlt.end()-1; vii+1 != rlt.begin(); vii--){
-         real_rlt.push_back(*vii);
-       }
-       return real_rlt;
-     }
- };
+ * Binary Tree Level Order Traversal II 
+ * Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+ * Solution: easy, just be careful
+ */
+class Solution {
+public:
+    vector<vector<int> > levelOrderBottom(TreeNode *root);
+};
+
+void search(vector<TreeNode*> & nodes, vector<vector<int> > & rlt){
+	vector<TreeNode*> next_nodes;
+	for (auto node:nodes){
+		if (node->left != NULL)
+			next_nodes.push_back(node->left);
+		if (node->right != NULL)
+			next_nodes.push_back(node->right);
+	}
+	if (next_nodes.size()>0)
+		search(next_nodes, rlt);
+	vector<int> vc;
+	for (auto node:nodes)
+		vc.push_back(node->val);
+	if (vc.size()>0)
+		rlt.push_back(vc);
+}
+
+vector<vector<int> > Solution::levelOrderBottom(TreeNode * root){
+	vector<vector<int> > rlt;
+	if (root == NULL)
+		return rlt;
+	vector<TreeNode*> tmp(1, root);
+	search(tmp, rlt);
+	return rlt;
+}
