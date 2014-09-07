d6bc8785429943c32dd1a09f423be86f0ee58303
landhero <raindrops_fall@sina.com>
2014-09-07 16:53:12 +0800

    largest rectangle in hisogram: merge into one loop

--- src/cpp/largest-rectangle-in-histogram.cpp
+++ src/cpp/largest-rectangle-in-histogram.cpp
@@ -1,35 +1,31 @@
 /**
- * reference: http://www.geeksforgeeks.org/largest-rectangle-under-histogram/
- * @author Hansheng Zhang (http://zhanghs.com)
- */
+* Largest Rectangle in Histogram
+* Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
+* Solution: calculate the area with s.top() as its minimal height
+* Reference: http://www.geeksforgeeks.org/largest-rectangle-under-histogram/
+*/
+
 class Solution {
 public:
-    int largestRectangleArea(vector<int> &);
+    int largestRectangleArea(vector<int> &height);
 };
 
-int Solution::largestRectangleArea(vector<int> & height){
-    int rlt = 0;
-    int i = 0, n=height.size();
-    stack<int> s;
-    while (i<n){
-        if (s.empty() || height[i]>=height[s.top()]){
-            s.push(i++);
-            continue;
-        }
-        int t = s.top();
-        s.pop();
-        int width = s.empty() ? i: i-s.top()-1;
-        int area = height[t] * width;
-        if ( area > rlt )
-            rlt = area;
-    }
-    while (!s.empty()){
-        int t = s.top();
-        s.pop();
-        int width = s.empty() ? n : n-s.top()-1;
-        int area = width * height[t];
-        if (area > rlt)
-            rlt = area;
-    }
-    return rlt;
+int Solution::largestRectangleArea(vector<int> &height){
+	int rlt = 0;
+	int n = height.size();
+	stack<int> s;
+	int i = 0;
+	while (i<n || !s.empty()){
+		if (i<n &&(s.empty() || height[i]>=height[s.top()])  ){
+			s.push(i++);
+			continue;
+		}
+		int t = s.top();
+		s.pop();
+		int width = s.empty() ? i:i-s.top()-1;
+		int area = height[t]*width;
+		if (area > rlt)
+			rlt = area;
+	}
+	return rlt;
 }
